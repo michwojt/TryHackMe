b2cf4cbb72843e64bb6437110ce6c4a2221b0931
michwojt <xy97@wp.pl>
2024-11-28 13:39:01 +0100

    Add gobuster with extensions and nikto

--- ServicesEnumeration.sh
+++ ServicesEnumeration.sh
@@ -1,6 +1,11 @@
 #Find hidden folders
 #Replace target and path to wordlist
 gobuster dir -u http://target -w /path
+#Add file extensions for searching
+gobuster dir -u http://target -w /path -x php,sh,txt,cgi,html,js,py,css
 
 #Enumerate users and other information from the Samba service on the target machine
 enum4linux -a target | tee enum4.log
+
+#Find vulnerabilities using nikto
+nikto -h http://10.10.x.x
