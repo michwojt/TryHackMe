92f601ee4ab9a41dc14a4f3cdd6960e370e045bb
michwojt <xy97@wp.pl>
2025-01-21 13:31:18 +0100

    Add subdomain discovery

--- ServicesEnumeration.sh
+++ ServicesEnumeration.sh
@@ -4,6 +4,9 @@ gobuster dir -u http://target -w /path
 #Add file extensions for searching
 gobuster dir -u http://target -w /path -x php,sh,txt,cgi,html,js,py,css
 
+#Find subdomains
+ffuf -u http://creative.thm -w /usr/share/dirb/wordlists/big.txr -H "Host: FUZZ.creative.thm" -fw 8
+
 #Enumerate users and other information from the Samba service on the target machine
 enum4linux -a target | tee enum4.log
 
